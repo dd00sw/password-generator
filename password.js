7efbe55ae48d1a7e0820f494c562cda80bf67734
dd00sw <dd015.2@icloud.com>
2025-02-06 10:53:28 +0300

    fix: add special symbols

--- password.js
+++ password.js
@@ -1,14 +1,16 @@
 
+let element = ".,/\!@#$%^&*()_+=-{}[];:|<>";
 let language = "abcdefghijklmnopqrstuvwxyz";
 let language2 = language.toUpperCase();
 let language3 = language.concat(language2);
+let elements = element.concat(language3);
 function password(length) {
     let result = "";
     for (let i = 0; i < length; i++) {
-        let rnd = language3[Math.floor(Math.random() * language3.length)];
+        let rnd = elements[Math.floor(Math.random() * elements.length)];
         result += rnd
 
     } return result;
 }
-console.log(password(40));
+console.log(password(10));
 
